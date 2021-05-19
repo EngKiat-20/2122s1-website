acf1658c097944a9f8ce87b05f17872f21dd4f95
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-05-19 10:54:49 +0800

    Update connector.mjs

--- scripts/modules/connector.mjs
+++ scripts/modules/connector.mjs
@@ -39,7 +39,7 @@ function makeFromDurationUrl(baseUrl, from, duration) {
  * @param duration the amount of minutes from the `from` parameter
  * @returns a Promise that resolves with the Error Chart's payload.
  */
-export function getErrorChartPayload(from, duration) {
+export default function getErrorChartPayload(from, duration) {
     const url = makeFromDurationUrl(`${host}${END_POINTS.errors}`, from, duration);
     return getChartPayload(url);
 }
