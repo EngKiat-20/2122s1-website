57bf0c2e8bc1e4c5da39ec2612581f8dcd65e6f7
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-05-20 18:24:41 +0800

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,4 +1,24 @@
 var errorColor;
-var defaultColor = "#0000ff";
+var defaultColor = "#ff0000";
 
 window.addEventListener("load", startup, false);
+function startup() {
+    errorColor = document.querySelector("#errorColor");
+    errorColor.value = defaultColor;
+    errorColor.addEventListener("input", updateFirst, false);
+    errorColor.addEventListener("change", updateAll, false);
+    errorColor.select();
+  }
+
+  function updateFirst(event) {
+    var p = document.querySelector("p");
+  
+    if (p) {
+      p.style.color = event.target.value;
+    }
+  }
+  function updateAll(event) {
+    document.querySelectorAll("p").forEach(function(p) {
+      p.style.color = event.target.value;
+    });
+  }
