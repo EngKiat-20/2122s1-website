8a202133dfc883c67637700c7da3b7df35a4dfd2
EngKiat-20 <engkiat.20@ichat.sp.edu.sg>
2021-05-25 17:05:26 +0800

    Update configuration.mjs

--- configuration.mjs
+++ configuration.mjs
@@ -8,11 +8,12 @@ window.addEventListener("load", startup, false);
 function startup() {
     errorColor = document.querySelector("#errorColor");
     errorColor.value = errorDefaultColor;
-    errorColor.addEventListener("input", updateErrorColor, false);
+    errorColor.addEventListener("input", setErrorColor, false);
+    errorColor.addEventListener("change", updateErrorColor, false);
     errorColor.select();
   }
 
-  function updateErrorColor(event) {
+  function setErrorColor(event) {
     var errorChart = charts.errorRate.options.colors[0];
     
     console.log(errorChart)
